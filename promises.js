ec170fa15c93da2c9fc266b91152e0e5badca4f9
NerellaRajeshwari <36570159+NerellaRajeshwari@users.noreply.github.com>
2018-11-16 20:54:40 +0530

    Create promises.js

---
+++ promises.js
@@ -0,0 +1,40 @@
+example for promises:
+
+let clean=function()
+{
+    return new Promise(function(resolve,reject)
+    {
+        resolve("cleaned the room ");
+    });
+};
+
+let remove=function(msg)
+{
+    return new Promise(function(resolve,reject)
+    {
+        resolve(msg +"remove garbage ");
+    });
+};
+let chocolate=function(msg)
+{
+    return new Promise(function(resolve,reject)
+    {
+        resolve(msg +"chocolate treat ");
+    });
+};
+clean().then(function(result)
+{
+    return remove(result);
+}).then(function(result)
+{
+    return chocolate(result);
+}).then(function(result)
+{
+    console.log(result+" all functions completed");
+})
+
+
+output:
+
+PS C:\Users\Rajeshwary\Desktop\my-dream-app\e2e> node merge.js
+cleaned the room remove garbage chocolate treat  all functions completed
